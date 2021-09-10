7d0a7e5e29af18494999cb28a085a0c40ae685a6
송병섭 <tumtum@naver.com>
2021-09-10 11:52:06 +0900

    key 해시 값 추출

--- app/src/main/java/com/nepplus/finalproject/LoginActivity.kt
+++ app/src/main/java/com/nepplus/finalproject/LoginActivity.kt
@@ -1,8 +1,12 @@
 package com.nepplus.finalproject
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.nepplus.finalproject.databinding.ActivityLoginBinding
+import java.security.MessageDigest
 
 class LoginActivity : BaseActivity() {
 
@@ -21,5 +25,14 @@ class LoginActivity : BaseActivity() {
 
     override fun setValues() {
 
+        val info = packageManager.getPackageInfo(
+            "com.nepplus.finalproject",
+            PackageManager.GET_SIGNATURES
+        )
+        for (signature in info.signatures) {
+            val md: MessageDigest = MessageDigest.getInstance("SHA")
+            md.update(signature.toByteArray())
+            Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+        }
     }
 }
