a33fbce009e0b0e68324b6dd465227cd889b6b9c
송병섭 <tumtum@naver.com>
2021-09-10 15:38:08 +0900

    회원가입 화면 추가 및 데이터바인딩 작업

---
+++ app/src/main/java/com/nepplus/finalproject/SignUpActivity.kt
@@ -0,0 +1,26 @@
+package com.nepplus.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.finalproject.databinding.ActivitySignUpBinding
+
+class SignUpActivity : BaseActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
