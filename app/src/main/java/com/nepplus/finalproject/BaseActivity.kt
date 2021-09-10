50a5d3753a54de9c20d7c924473dc956c07fb52d
송병섭 <tumtum@naver.com>
2021-09-10 16:28:02 +0900

    ServerAPIService.kt 작성 완료. 베이스 액티비티에 retrofit, apiService 변수 추가하여 모든 화면에서 사용 가능하게

--- app/src/main/java/com/nepplus/finalproject/BaseActivity.kt
+++ app/src/main/java/com/nepplus/finalproject/BaseActivity.kt
@@ -3,15 +3,25 @@ package com.nepplus.finalproject
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.nepplus.finalproject.web.ServerAPI
+import com.nepplus.finalproject.web.ServerAPIService
+import retrofit2.Retrofit
 
 abstract class BaseActivity: AppCompatActivity() {
 
     lateinit var mContext: Context
 
+    private lateinit var retrofit: Retrofit
+
+    lateinit var apiService: ServerAPIService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mContext = this
+
+        retrofit = ServerAPI.getRetrofit()
+        apiService = retrofit.create(ServerAPIService::class.java)
     }
 
     abstract fun setupEvents()
---
+++ app/src/main/java/com/nepplus/finalproject/web/ServerAPIService.kt
@@ -0,0 +1,18 @@
+package com.nepplus.finalproject.web
+
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.PUT
+
+interface ServerAPIService {
+
+    @FormUrlEncoded
+    @PUT("/user")
+    fun putRequestSignUp(
+        @Field("email") email: String,
+        @Field("password") pw: String,
+        @Field("nick_name") nickname: String) {
+
+    }
+
+}
