69e808185391a09543f236be63e832ea4db3f0bb
송병섭 <tumtum@naver.com>
2021-09-14 10:41:03 +0900

    리스트 아이템에 약속 장소 지도를 크게 보기 위한 UI 설정

---
+++ app/src/main/java/com/nepplus/finalproject/ViewMapActivity.kt
@@ -0,0 +1,20 @@
+package com.nepplus.finalproject
+
+import android.os.Bundle
+
+class ViewMapActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_map)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
