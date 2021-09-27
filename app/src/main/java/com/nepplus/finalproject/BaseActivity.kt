3fbcbbe2a95353983cf44c2c4aa07e1490ed446d
송병섭 <tumtum@naver.com>
2021-09-27 23:07:27 +0900

    #11 PopUp 액티비티 UI 수정 작업 진행 중. BaseActivity 에 커스텀액션바 내용 추가

--- app/src/main/java/com/nepplus/finalproject/BaseActivity.kt
+++ app/src/main/java/com/nepplus/finalproject/BaseActivity.kt
@@ -2,7 +2,11 @@ package com.nepplus.finalproject
 
 import android.content.Context
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.nepplus.finalproject.web.ServerAPI
 import com.nepplus.finalproject.web.ServerAPIService
 import retrofit2.Retrofit
@@ -15,6 +19,9 @@ abstract class BaseActivity: AppCompatActivity() {
 
     lateinit var apiService: ServerAPIService
 
+    lateinit var barTitleTxt: TextView
+    lateinit var barAddListImg: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,10 +29,26 @@ abstract class BaseActivity: AppCompatActivity() {
 
         retrofit = ServerAPI.getRetrofit(mContext)
         apiService = retrofit.create(ServerAPIService::class.java)
+
+        supportActionBar?.let {
+            setActionBar()
+        }
     }
 
     abstract fun setupEvents()
 
     abstract fun setValues()
+
+    fun setActionBar() {
+        val defaultBar = supportActionBar!!
+        defaultBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        defaultBar.setCustomView(R.layout.action_bar_custom)
+
+        val toolBar = defaultBar.customView.parent as Toolbar
+        toolBar.setContentInsetsAbsolute(0,0)
+
+        barTitleTxt =defaultBar.customView.findViewById(R.id.barTitleTxt)
+        barAddListImg =defaultBar.customView.findViewById(R.id.barAddListImg)
+    }
 
 }
